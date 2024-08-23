437d17f1caac9a9513c6185caf69eccbc0efa4bd
Moneybag01 Bull <Kryptee999@gmail.com>
2024-08-23 14:50:48 +0000

    Initial commit

---
+++ scripts/upgrade.js
@@ -0,0 +1,40 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpclink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpclink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  // Replace with the address of your deployed proxy contract
+  const proxyAddress = "0xFE73E2C56eb050686F4bC07083b61d96204FD4c6"; 
+
+  // Replace with the address of your new implementation contract
+  const newImplementationAddress = "0x7D3C68d37fF6DB7c05a417C2b0873fe2734E5EaF"; 
+  
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("SwisstronikProxy");
+  const proxyContract = contractFactory.attach(proxyAddress);
+
+  // Prepare the data for the upgradeTo function call
+  const functionName = "upgradeTo";
+  const upgradeToTxData = proxyContract.interface.encodeFunctionData(functionName, [newImplementationAddress]);
+
+  // Send the shielded transaction to upgrade the proxy to the new implementation
+  const upgradeTx = await sendShieldedTransaction(signer, proxyAddress, upgradeToTxData, 0);
+  await upgradeTx.wait();
+
+  console.log("Proxy upgraded successfully. Transaction Receipt: ", upgradeTx);
+}
+
+main().catch((error) => {
+  console.error("Error during upgrade:", error);
+  process.exitCode = 1;
+});
